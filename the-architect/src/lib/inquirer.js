3cce0460ca8bb693e07d4a9ad9e52bd033c9893f
Your Namethaisribeiro <thaisnribeiro2008@gmail.com>
2018-09-19 19:59:03 -0300

    Criação de menus

--- the-architect/src/lib/inquirer.js
+++ the-architect/src/lib/inquirer.js
@@ -2,8 +2,14 @@ import inquirer from 'inquirer'
 
 export default {
   installExtensions: () => {
+    const frameworks = ['vue', 'react']
     const questions = {
-      type: 'checkbox-plus'
+      type: 'checkbox-plus',
+      name: 'extensions',
+      message: `Speak my consecrated, we have here a package of
+      extensions for your VSCode,choose your frameworks
+      to have an idea of ​​what will be suitable for you`,
+      default: ['vue']
     }
   }
 }
